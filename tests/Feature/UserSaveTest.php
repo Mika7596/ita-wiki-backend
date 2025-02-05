420dd897f9f59b67c3665d5a26b5f4966af875f3
diegdar <diegdarch@hotmail.com>
2025-02-05 20:17:30 +0100

    test: Add feature tests for user saving functionality

---
+++ tests/Feature/UserSaveTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserSaveTest extends TestCase
+{
+    public function testCanUserBeSaved()
+    {
+        $data = [
+            'github_id' => '1234567890123456789012345',
+        ];
+
+        $response = $this->postJson(route('user.save'), $data);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', [
+            'github_id' => $data['github_id'],
+        ]);
+    }
+
+    public function testValidateErrorMinimumGithubCharacters()
+    {
+        $data = [
+            'github_id' => '12345',
+        ];
+
+        $response = $this->postJson(route('user.save'), $data);
+
+        $response->assertStatus(422);
+    }
+
+    public function testValidateErrorEmptyGithubId()
+    {
+        $data = [
+            'github_id' => '',
+        ];
+
+        $response = $this->postJson(route('user.save'), $data);
+
+        $response->assertStatus(422);
+    }
+
+    public function testValidateErrorMissingGithubId()
+    {
+        $response = $this->postJson(route('user.save'));
+
+        $response->assertStatus(422);
+    }
+
+}
