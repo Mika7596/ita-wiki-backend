7da1c72be014543ac33e2b760bf8baac8a2d78a6
diegdar <diegdarch@hotmail.com>
2025-02-07 10:33:36 +0100

    feat(user): add nickname field to User model and migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,6 +11,8 @@ return new class () extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->string('github_id')->primary();
+            $table->string('nickname')->nullable(false);
+            $table->timestamps();
         });
     }
 
