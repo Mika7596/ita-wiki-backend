1cde4823cc2ca4578c20ab64e9dc0df14c85bd06
CotaGM <cotadiseno@gmail.com>
2025-02-25 19:35:22 +0100

    Adding some extra test related to the terms and conditions terms

--- tests/Feature/RoleControllerTest.php
+++ tests/Feature/RoleControllerTest.php
@@ -53,4 +53,41 @@ class RoleControllerTest extends TestCase
         ]);
     }
 
+    /*Terms and condition test
+    public function testCannotSignUpWithoutAcceptingTerms(): void
+{
+    $random_github_id = random_int(1, 10000000);
+
+    // Enviar solicitud sin los términos aceptados
+    $response = $this->post('/api/users/user-signedin-as', [
+        'github_id' => $random_github_id,
+        'terms_accepted' => false // No aceptó los términos
+    ]);
+
+    $response->assertStatus(400) // Se espera un error (400)
+        ->assertJson([
+            'message' => 'You must accept the terms and conditions before signing up.',
+        ]);
+
+        public function testSignupWithAcceptedTerms(): void
+{
+    $random_github_id = random_int(1, 10000000);
+
+    // Enviar solicitud con los términos aceptados
+    $response = $this->post('/api/users/user-signedin-as', [
+        'github_id' => $random_github_id,
+        'terms_accepted' => true // Aceptó los términos
+    ]);
+
+    $response->assertStatus(201) // Se crea el usuario correctamente
+        ->assertJson([
+            'message' => 'Role not found. Created as new anonymous user.',
+            'role' => [
+                'github_id' => $random_github_id,
+                'role' => 'anonymous',
+            ]
+        ]);
+}
+}*/
+
 }
