621048d3774922020e6a2d66ef3022293cda9198
CotaGM <cotadiseno@gmail.com>
2025-03-03 19:04:19 +0100

    swagger anotations for controller endpoint user-signedin-as

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -4,10 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Role;
-
+    /**
+     * @OA\Info(
+     *  title="Swagger Documentation for ITA-Wiki",
+     *  version="1.0.0.0",
+     *  description="Project ITA-Wiki documentation wall"
+     * )
+     */
 class RoleController extends Controller
 {
-    //
+    /   /**
+    * @OA\Get(
+    *     path="/api/roles",
+    *     summary="Retrieve a role by GitHub ID",
+    *     description="Fetches a role using the provided GitHub ID. If the role does not exist, it creates a new role for the user as anonymous.",
+    *     @OA\Response(
+    *         response=200,
+    *         description="Role found",
+    *         @OA\JsonContent(
+    *             type="object",
+    *             @OA\Property(property="message", type="string", example="Role found."),
+    *             @OA\Property(
+    *                 property="role",
+    *                 type="object",
+    *                 ref="#/components/schemas/Role"
+    *             )
+    *         )
+    *     ),
+    *     @OA\Response(
+    *         response=201,
+    *         description="Role not found, created as a new anonymous user",
+    *         @OA\JsonContent(
+    *             type="object",
+    *             @OA\Property(property="message", type="string", example="Role not found. Created as new anonymous user."),
+    *             @OA\Property(
+    *                 property="role",
+    *                 type="object",
+    *                 ref="#/components/schemas/Role"
+    *             )
+    *         )
+    *     )
+    * )
+    */
+
     public function getRoleByGithubId(Request $request)
     {
         $request->headers->set('Accept', 'application/json');
