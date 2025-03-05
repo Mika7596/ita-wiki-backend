a3ef5b76a52533c3d0f778eb4b17bac086fa4712
develoopzz <develoopzz00@gmail.com>
2025-03-05 17:40:16 +0100

    Swagger syntax errors on resource model

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -19,8 +19,8 @@ use Illuminate\Database\Eloquent\Model;
      *      @OA\Property(property="url", type="string", nullable=true, example="https://www.hola.com", format="url"),
      *      @OA\Property(property="category", type="string", enum={"Node","React","Angular","Javascript","Java","Fullstack PHP", "Data Science","BBDD"}, example="Node"),
      *      @OA\Property(property="theme", type="string", enum={"All","Components","UseState & UseEffect","Eventos","Renderizado condicional","Listas", "Estilos","Debugging", "React Router"}, example="All"),
-     *      @OA\Property(property="type", type="string", enum={"Video","Cursos,"Blog"}, example="Video"),
-     *      @OA\Property(property="votes" type="integer", example = 1)
+     *      @OA\Property(property="type", type="string", enum={"Video","Cursos","Blog"}, example="Video"),
+     *      @OA\Property(property="votes", type="integer", example = 1)
      * )
      */
 class Resource extends Model
