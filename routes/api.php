12230b405c193f6af54de1967cc796196924eda3
Xavi <149520332+llotjer@users.noreply.github.com>
2025-03-13 18:27:50 +0100

    feat: add CreateRoleRequest for role creation validation and update API routes
    Co-authored-by: Diego Chacon <diegdar@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,5 @@ Route::get('/bookmarks/{github_id}', [BookmarkController::class,'getStudentBookm
 Route::post('/bookmarks', [BookmarkController::class,'createStudentBookmark'])->name('bookmark.create');
 
 Route::delete('/bookmarks', [BookmarkController::class,'deleteStudentBookmark'])->name('bookmark.delete');
+
+Route::post('/roles', [RoleController::class, 'createRole'])->name('roles.create');
