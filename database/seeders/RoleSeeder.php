3d52cdc7079a9a3bf50b8846470e42de4011ae86
frankpulido <frankpulido@me.com>
2025-03-20 19:51:42 +0100

    all frontend students have a role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,15 +14,24 @@ class RoleSeeder extends Seeder
     {
         Role::create(['github_id' => 1, 'role' => 'superadmin']);
 
-        Role::create([
-            'github_id' => 6729608,
-            'role' => 'student',
-        ]);
+        // Oreana is admin
+        Role::create(['github_id' => 92450839, 'role' => 'admin']);
 
-        Role::create([
-            'github_id' => 47982114,
-            'role' => 'student',
-        ]);
+        // Frontend team members
+        $studentGithubIds = [
+            6729608,
+            47982114,
+            132897973,
+            132501110,
+            119063441
+        ];
+
+        foreach ($studentGithubIds as $githubId) {
+            Role::create([
+                'github_id' => $githubId,
+                'role' => 'student',
+            ]);
+        }
 
         Role::factory(20)->create();
     }
