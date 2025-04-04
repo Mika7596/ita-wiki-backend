700dc1f64e7d2a8927d6e6d35dc74254c8851a10
frankpulido <frankpulido@me.com>
2025-04-04 14:52:56 +0200

    updated resource model swagger documentation

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -20,7 +20,8 @@ use Illuminate\Database\Eloquent\Model;
      *      @OA\Property(property="category", type="string", enum={"Node","React","Angular","JavaScript","Java","Fullstack PHP", "Data Science","BBDD"}, example="Node"),
      *      @OA\Property(property="theme", type="string", enum={"All","Components","UseState & UseEffect","Eventos","Renderizado condicional","Listas", "Estilos","Debugging", "React Router"}, example="All"),
      *      @OA\Property(property="type", type="string", enum={"Video","Cursos","Blog"}, example="Video"),
-     *      @OA\Property(property="votes", type="integer", example = 1)
+     *      @OA\Property(property="bookmark_count", type="integer", example = 1),
+     *      @OA\Property(property="like_count", type="integer", example = 1)
      * )
      */
 class Resource extends Model
