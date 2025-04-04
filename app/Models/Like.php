94fa4d37d3b3406147ed4160c81bfb0b46f743aa
frankpulido <frankpulido@me.com>
2025-04-04 18:09:19 +0200

    swagger documentation for model Like

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -9,6 +9,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Observers\LikeObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
+/**
+ * @OA\Schema(
+ *     schema="Like",
+ *     type="object",
+ *     title="Like",
+ *     @OA\Property(property="id", type="integer", example=9),
+ *     @OA\Property(property="github_id", type="integer", description="Foreign key representing the GitHub ID of the user", example=6729608),
+ *     @OA\Property(property="resource_id", type="integer", description="Foreign key representing the ID of the liked resource", example=11),
+ *     @OA\Property(property="created_at", type="string", format="date-time", example="2025-03-17T19:23:41.000000Z"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", example="2025-03-17T19:23:41.000000Z")
+ * )
+ */
+
 #[ObservedBy([LikeObserver::class])]
 class Like extends Model
 {
