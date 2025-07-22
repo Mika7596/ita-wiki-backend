c7acbef0f68d4ba73e98178c2235b3d73ceb4a5e
CristinaSoy <ccardonaaltes@hotmail.com>
2025-07-22 09:18:35 +0200

    github_id_name added to new user migration

--- database/migrations/2025_07_21_185213_add_github_id_to_users_table.php
+++ database/migrations/2025_07_21_185213_add_github_id_to_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->bigInteger('github_id')->unsigned()->unique();
+            $table->string('github_user_name');
         });
     }
 
